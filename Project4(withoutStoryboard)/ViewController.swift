87410fee020684957673a15591033b8d77f642c8
Alex Bonney <bonney977@gmail.com>
2021-07-29 05:21:34 +0300

    Initial Commit

---
+++ Project4(withoutStoryboard)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project4(withoutStoryboard)
+//
+//  Created by Alexander Bonney on 7/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
